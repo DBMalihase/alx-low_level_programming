b12663c8619726cc0aceba3e5c8bc27686540deb
DBMalihase <dennismalihase@gmail.com>
2022-09-08 09:22:53 -0700

    C-programming intro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- * main -Entry point
- *
- * Description: using the main function 
- *
- * Return: 0 (success)
+ * main - Entry point
+ * Description: using the main function
+ * Return: 0
  */
 int main(void)
 {
