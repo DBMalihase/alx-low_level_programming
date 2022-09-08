2dbc9a7260bf18a05504313ad398fb6e4756e7c7
DBMalihase <dennismalihase@gmail.com>
2022-09-08 11:32:29 -0700

    C-programming intro

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Entry point
+ * main - Entry point for function
+ * Description: Places our quote to stderr instead of stdout
  * Return: 1
  */
 int main(void)
 {
-	write(2,
-			"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, quote, 59);
 	return (1);
 }
