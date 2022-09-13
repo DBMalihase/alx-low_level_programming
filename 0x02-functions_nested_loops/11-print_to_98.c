fd03c785ad27e2a9573eb98110ef735eeb6857e5
DBMalihase <dennismalihase@gmail.com>
2022-09-13 15:43:24 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - print all natural numbers from input to 98, 
- * in order separated by a comma followed by a space
+ * print_to_98 - print all natural numbers from input to 98
  * @n: the number to begin counting at
  */
 
