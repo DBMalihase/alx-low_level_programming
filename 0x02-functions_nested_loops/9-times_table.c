f2a44ff296e5e0c45d10be8f0288e2462d31ceb0
DBMalihase <dennismalihase@gmail.com>
2022-09-13 15:05:28 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,51 +1,28 @@
 #include "main.h"
 
 /**
- * print_times_table - times table function
+ * print_times_table - prints the 9 times table, starting with 0
  * @n: integer to return
  */
 
-void print_times_table(int n)
+void times_table(void)
 {
-	int i;
-	int j;
+	int a, b, result;
 
-	if (n <= 15 && n >= 0)
+	for (a = 0; a <= 9; a++)
 	{
-		for (i = 0; i <= n; i++)
+		_putchar('0');
+		for (b = 1; b <= 9; b++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-			int prod = j * i;
-
-			if (j == 0)
-			{
-				_putchar('0');
-			} else if (prod <= 9)
-			{
-				_putchar(',');
+			_putchar(',');
+			_putchar(' ');
+			result = a * b;
+			if (result <= 9)
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + prod);
-			} else if (prod > 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + (prod / 100));
-				_putchar('0' + ((prod / 10) % 10));
-				_putchar('0' + (prod % 10));
-			} else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (prod / 10));
-				_putchar('0' + (prod % 10));
-			}
+			else
+				_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
 		}
 		_putchar('\n');
-		}
 	}
 }
-
