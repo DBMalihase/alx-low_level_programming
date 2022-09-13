70ac49d8cb47ffd39c89379f02e67af1b47c1d21
DBMalihase <dennismalihase@gmail.com>
2022-09-13 15:29:13 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include "main.h"
 #include "6-abs.c"
+#include <stdio.h>
 
 /**
  * print_to_98 - print all natural numbers from input 98, in order separated by a coma followed by a space
@@ -11,7 +11,6 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while (n > 98)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
