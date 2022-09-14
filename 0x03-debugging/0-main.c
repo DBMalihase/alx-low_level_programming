83c7ece21a27f55adf371f6dd671bccec8db26e3
DBMalihase <dennismalihase@gmail.com>
2022-09-14 09:37:00 -0700

    C project debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Print if integer is positive or negative
+ * Return: 
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
+
+
