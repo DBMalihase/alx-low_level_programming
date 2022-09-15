904f5fc34be12936084ca394a74193f1be20b992
DBMalihase <dennismalihase@gmail.com>
2022-09-15 13:01:21 -0700

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: the number to be checked
+ * @b: the number to be checked
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
