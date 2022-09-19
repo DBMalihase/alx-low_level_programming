77121c03c98f269e023832e1e472395298054899
DBMalihase <dennismalihase@gmail.com>
2022-09-19 10:42:14 -0700

    Pointers , Arrays , Strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - print strings in par position
+ * @str: variable which holds the string
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int c = 0;
+	int i;
+
+	while (str[c] != '\0')
+	{
+		c++
+	}
+	for (i = 0; i < c; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
