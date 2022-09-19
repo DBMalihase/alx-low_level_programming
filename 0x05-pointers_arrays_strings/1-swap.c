81101e4a2232b937295ba040468e38cc209a0d82
DBMalihase <dennismalihase@gmail.com>
2022-09-19 09:15:45 -0700

    Pointers , Arrays , Strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * @a: input value
+ * b@: input another value
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
