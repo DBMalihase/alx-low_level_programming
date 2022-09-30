b89d9de3acc76dab9a1e0032f5d787f749293e9c
DBMalihase <dennismalihase@gmail.com>
2022-09-30 09:58:43 -0700

    Argc, Argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
