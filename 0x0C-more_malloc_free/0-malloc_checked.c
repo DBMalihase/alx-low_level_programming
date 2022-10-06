1ff90abcc9178133a749f901fa918997ef88d3bc
DBMalihase <dennismalihase@gmail.com>
2022-10-06 13:07:13 -0700

    More malloc free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * malloc_checked - performs a malloc, if failed exits
+ * @b: number of bytes to allocate
+ * Return: pointer to beginning of new memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ret;
+
+	ret = malloc(b);
+	if (ret == NULL)
+		exit(98);
+	return (ret);
+}
