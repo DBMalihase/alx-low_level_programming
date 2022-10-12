353706cbf76854f5fdc308e70e8bcff3330b48ca
DBMalihase <dennismalihase@gmail.com>
2022-10-12 11:33:24 -0700

    Function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
 
 int _int_index(int *array, int size, int (*cmp)(int));
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *name));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
