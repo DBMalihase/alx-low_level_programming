5f858135bf9442de0169324f9825ad6884ecac38
DBMalihase <dennismalihase@gmail.com>
2022-10-12 12:03:13 -0700

    Function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,17 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (size <= 0 || array == NULL || cmp == NULL)
+	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	while (i < size)
 	{
 		if (cmp(array[i]))
 			return (i);
+		i++;
 	}
 	return (-1);
 }
