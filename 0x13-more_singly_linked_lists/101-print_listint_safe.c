78a9d31dacc1398ea1d2dec6f611a8299540c534
DBMalihase <dennismalihase@gmail.com>
2022-10-25 14:11:00 -0700

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint_safe - tprints a listint_t linked list
+ * @head: head node
+ * Return: the number of nodes in list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int difference, index, number;
+	void *address;
+
+	index = 0;
+
+	while (head != NULL)
+	{
+		difference = head - head->next;
+		index++;
+		number = head->n;
+		printf("[%p] %i\n", (void *)head, number);
+		if (difference > 0)
+			head = head->next;
+		else
+		{
+			address = head->next;
+			number = head->next->n;
+			printf("-> [%p] %i\n", address, number);
+			break;
+		}
+	}
+	return (index);
+}
